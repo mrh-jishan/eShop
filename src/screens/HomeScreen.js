97d2d42f278c4d477a7f91560f9803dc1b84c372
robin-hassan <robiul.hassan12102@gmail.com>
2020-10-13 19:35:28 +0800

    added scope for user auth

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,12 +13,14 @@ const HomeScreen = ({ navigation }) => {
 
   useEffect(() => {
     GoogleSignin.configure({
-      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      scopes: ['https://www.googleapis.com/auth/contacts.readonly',
+        'https://www.googleapis.com/auth/user.birthday.read',
+        'https://www.googleapis.com/auth/user.gender.read',
+        'https://www.googleapis.com/auth/user.phonenumbers.read'], // what API you want to access on behalf of the user, default is email and profile
       webClientId: '103375050274-lp2q3th8bmi10nr51kscsktg4svn4jcr.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
       iosClientId: '103375050274-lp2q3th8bmi10nr51kscsktg4svn4jcr.apps.googleusercontent.com'
     });
-    console.log('run effect');
   }, []);
   const [isSigninInProgress, setIsSigninInProgress] = useState(false);
 
@@ -45,25 +47,20 @@ const HomeScreen = ({ navigation }) => {
   };
 
   return (
-    <Background style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
+    <Background>
       <Logo />
       <Header>An alternative E-commerce</Header>
       <Paragraph>
         The easiest way to start with your amazing application.
     </Paragraph>
 
-      {/* <View style={styles.sectionContainer}> */}
       <GoogleSigninButton
         style={{ width: 192, height: 48 }}
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Dark}
         onPress={_signIn}
         disabled={isSigninInProgress} />
-      {/* </View> */}
+
     </Background>
   )
 }
